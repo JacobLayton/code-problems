472347232c5e3b204ded4aca2f99c83c9f173169
JacobLayton <jlayjlay3@gmail.com>
2019-09-05 19:09:34 -0700

    Completed divisible sum pairs problem

---
+++ hackerrank/Algorithms/algorithms/divisibleSumPairs.js
@@ -0,0 +1,14 @@
+function divisibleSumPairs(n, k, ar) {
+  let count = 0;
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+let array = [1, 3, 2, 6, 1, 2];
+console.log(divisibleSumPairs(6, 3, array));
